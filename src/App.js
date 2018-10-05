a57cdaa2fcc5f2e1db161b2d573e2aea095fd748
Deordiieva <olena.deordiieva@accenture.com>
2018-10-05 22:36:20 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
-import {DetailedCard} from './DetailedCard';
+import { DetailedCard } from './DetailedCard';
 import logo from './logo.svg';
 import axios from 'axios';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 class Card extends Component {
-  favorites = []; 
+  favorites = [];
   addToFavorites = (item) => {
-      axios.get(item).then(resp=>{
+    axios.get(item).then(resp => {
       this.props.addFavorites(resp.data);
     })
   }
-  render(){
-      return(
-    <div className="card">
+  render() {
+    return (
+      <div className="card">
+          <img className="img" src={this.props.card.avatar_url} alt="img" />
         <div className="card-body">
-            <img className="img" src={this.props.card.avatar_url} alt="img"/>
-      </div>
-      <div className="card-body">
-        <Link to={`/user/${this.props.card.login}`} className="card-title">{this.props.card.login}</Link>
-        <button ref="btn" className="btn btn-primary" onClick={() => {this.addToFavorites(this.props.card.url)}}>+Add to favorites</button>
-      </div>
-    
-    </div>
+          <Link to={`/user/${this.props.card.login}`} className="card-title">{this.props.card.login}</Link>
+          <button ref="btn" className="btn btn-primary" onClick={() => { this.addToFavorites(this.props.card.url) }}>+Add to favorites</button>
+        </div>
 
-  );
+      </div>
+
+    );
   }
 
 };
@@ -36,97 +34,104 @@ const Favorite = (props) => {
     props.removeFromFav(item);
 
   }
-  return(
+  return (
     <div className="card">
+        <img className="img" src={props.favorite.avatar_url} alt="img" />
       <div className="card-body">
-        <img className="img" src={props.favorite.avatar_url} alt="img"/>
+        <Link to={`/user/${props.favorite.login}`} >{props.favorite.login}</Link>
+        <div><strong>{props.favorite.followers}</strong> followers</div>
+        {props.favorite.location ? <div><strong>Location: </strong>{props.favorite.location}</div> : null}
+        <button className="btn btn-primary" onClick={() => removeFromFav(props.favorite.id)}>Remove</button>
       </div>
-      <div className="card-body">
-        <h5 className="card-title">{props.favorite.login}</h5>
-        <div>{props.favorite.followers} followers</div>
-        <div>{props.favorite.location}</div>
-
-        <button className="btn btn-primary" onClick={()=>removeFromFav(props.favorite.id)}>- Remove from favorites</button>
-      </div>
-    
     </div>
-
   );
 };
 
 
-  const CardList = (props) => {
-    return (
-      <div>
-        {props.cards.sort((a, b) => a.name - b.name)
-                    .map(card => <Card card={card} addFavorites={props.addFavorites}
-                                        key={card.id.toString()}/>)}
-      </div>
-    )
-  }
-
-  const Favorites = (props) => {
-    return (
-      <div>
-        {props.favorites.sort((a, b) => a.name - b.name).map(favorite => <Favorite favorite={favorite} removeFromFav={props.removeFromFav} key={favorite.id.toString()}/>)}
-      </div>
-    )
-  }
-
-  class Form extends Component {
-    state = {userName: ''}
-    handleSubmit= (event) =>{
-      event.preventDefault();
-     axios.get(`https://api.github.com/search/users?q=${this.state.userName}`).then(resp=>{
-       this.props.onSubmit(resp.data);
-     })
-    }
-    render (){
-      return(
-        <form className="input-group mb-3" onSubmit={this.handleSubmit}>
-          <input type="text" className="form-control" value={this.state.userName}
-          onChange={(event)=> this.setState({userName: event.target.value})}
-          />
-          <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="submit">Submit</button>
-
-          </div>
-        </form>
+const CardList = (props) => {
+      return (
+        <div className="col">
+          {props.cards.length  ?  <p>Search results: </p> : null}
+          {props.cards.sort((a, b) => a.name - b.name)
+            .map(card => <Card card={card} addFavorites={props.addFavorites}
+              key={card.id.toString()} />)}
+        </div>
       )
-    }
+
+
+}
+
+const Favorites = (props) => {
+  return (
+    <div className="col">
+      {props.favorites.length ? <p>Favorites: </p> : null}
+      <div>
+        {props.favorites.sort((a, b) => a.name - b.name).map(favorite => <Favorite favorite={favorite} removeFromFav={props.removeFromFav} key={favorite.id.toString()} />)}
+      </div>
+    </div>
+  )
+}
+
+class Form extends Component {
+  state = { userName: '', submitted : false, result : [] }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.get(`https://api.github.com/search/users?q=${this.state.userName}`).then(resp => {
+      this.props.onSubmit(resp.data);
+      this.setState({
+        submitted: true,
+        result: resp.data.items
+      })
+    })
   }
+  render() {
+    return (
+      <div>
+      <form className="input-group mb-3" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Search GitHub user" className="form-control" value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" disabled={!this.state.userName} type="submit">Submit</button>
+        </div>
+      </form>
+      {(this.state.submitted && this.state.result.length === 0) ? <div className="alert alert-info" role="alert">No results found :(</div> : null }
+      </div>
+    )
+  }
+}
 class App extends Component {
   favoritesArr = [];
   state = {
-      cards: [],
-      favorites: []
-    }
+    cards: [],
+    favorites: []
+  }
   searchUser = (users) => {
-    this.setState({cards : users.items})
+    this.setState({ cards: users.items })
   }
 
   addFavorites = (favorite) => {
     let indexRemove;
     this.favoritesArr.push(favorite);
     this.state.cards.map((val, index) => {
-      if (val.id===favorite.id){
+      if (val.id === favorite.id) {
         indexRemove = index;
       }
     });
     this.state.cards.splice(indexRemove, 1);
-    this.setState({favorites: this.favoritesArr})
+    this.setState({ favorites: this.favoritesArr })
   }
 
   removeFromFav = (favorite) => {
     let indexRemove;
     this.favoritesArr.map((val, index) => {
-      if (val.id===favorite){
+      if (val.id === favorite) {
         indexRemove = index;
         this.state.cards.push(val);
       }
     });
     this.favoritesArr.splice(indexRemove, 1);
-    this.setState({favorites: this.favoritesArr});
+    this.setState({ favorites: this.favoritesArr });
 
   }
 
@@ -135,39 +140,30 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Quick search github users</h1>
         </header>
         <div className="App-intro">
           <div className="container">
-          <div className="row">
-          <div className="col">  
-            <Form onSubmit={this.searchUser}/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">  
-            <p>Search results: </p>
-            <BrowserRouter>
-            <Switch>
-            <Route path="/" exact render={
-              ()=> { return <CardList addFavorites={this.addFavorites}  cards={this.state.cards} />}}></Route>
-              <Route path="/user/:id" component={DetailedCard}></Route>
-
-                </Switch>
-
-              </BrowserRouter>
-
-            </div>
-            <div className="col">
-              <p>Favorites: </p>
-              <Favorites favorites={this.state.favorites} removeFromFav={this.removeFromFav}/>
-            </div>
-            </div>
             <div className="row">
-              <div className="col"></div>
+              <div className="col">
+                <Form onSubmit={this.searchUser} />
+              </div>
             </div>
+            <BrowserRouter >
+              <Switch>
+                <Route path="/" exact render={
+                  () => {
+                    return (
+                      <div className="row">
+                        <CardList addFavorites={this.addFavorites} cards={this.state.cards} />
+                        <Favorites favorites={this.state.favorites} removeFromFav={this.removeFromFav} /></div>
+                    )
+                  }}></Route>
+                <Route path="/user/:id" component={DetailedCard}></Route>
+              </Switch>
+            </BrowserRouter>
           </div>
-          
+
         </div>
       </div>
     );
--- src/DetailedCard.js
+++ src/DetailedCard.js
@@ -1,39 +1,38 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 
 export class DetailedCard extends Component {
-    state = {
-        fullCard : {}
-    }
-    constructor(props){
-        super(props);
-        axios.get(`https://api.github.com/users/${props.match.params.id}`).then(resp=>{
-            this.setState ({fullCard: resp.data})
-     })
-    }
-    render(){
-        return(
-      <div className="card">
-        <Link to="/" >Back</Link>
+  state = {
+    fullCard: {}
+  }
+  constructor(props) {
+    super(props);
+    axios.get(`https://api.github.com/users/${props.match.params.id}`).then(resp => {
+      this.setState({ fullCard: resp.data })
+    })
+  }
+  render() {
+    return (
+      <div className="row">
 
-       <div className="card-body">
-        <img className="img" src={this.state.fullCard.avatar_url} alt="img"/>
+        <div className="col">
+          <Link to="/" >Back</Link>
+          <div className="card">
+              <img className="img-large" src={this.state.fullCard.avatar_url} alt="img" />
+            <div className="card-body">
+              <h5 className="card-title"><strong>{this.state.fullCard.login}</strong></h5>
+              <div><strong>{this.state.fullCard.followers}</strong> followers</div>
+              {this.state.fullCard.name ? <div><strong>Name: </strong>{this.state.fullCard.name}</div> : null}
+              {this.state.fullCard.location ? <div><strong>Location:</strong> {this.state.fullCard.location}</div> : null}
+              {this.state.fullCard.company ? <div><strong>Company:</strong> {this.state.fullCard.company}</div> : null}
+              {this.state.fullCard.bio ? <div><strong>Bio:</strong> {this.state.fullCard.bio}</div> : null}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card-body">
-        <h5 className="card-title">{this.state.fullCard.login}</h5>
-        <div>Name: {this.state.fullCard.name} followers</div>
-        <div>{this.state.fullCard.followers} followers</div>
-        <div>Location: {this.state.fullCard.location}</div>
-        <div>Company: {this.state.fullCard.company}</div>
-        <div>Bio: {this.state.fullCard.bio}</div>
-
-      </div>
-      
-      </div>
-  
     );
-    }
-  
-  };
+  }
+
+};
